73906ceb35148245e8d10046b3586d02cc239312
Raul Hernandez <optimusgamer55hdz@gmail.com>
2026-04-17 10:03:59 -0600

    feat: added 3d example of structures conditional simple

---
+++ DPW/06_guia_practica/03example.php
@@ -0,0 +1,18 @@
+<?php
+$variable = 1;
+$conditional_value = 10;
+
+if ($variable > $conditional_value) {
+
+	echo "El valor de la variable es mayor que {$conditional_value}<br>";
+
+} elseif ($variable < $conditional_value) {
+
+	echo "El valor de la variable es menor que {$conditional_value}<br>";
+
+
+} else {
+
+	echo "El valor de la variable es igual a {$conditional_value}<br>";
+} 
+?>
