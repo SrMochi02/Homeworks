0fdc1de25c56e58f6b4597b450d9406190a27b4a
Raul Hernandez <optimusgamer55hdz@gmail.com>
2026-04-17 09:22:42 -0600

    feat: added example 2 of conditional structure

---
+++ DPW/06_guia_practica/02example.php
@@ -0,0 +1,15 @@
+<?php
+
+$variable = 1;
+$conditional_value = 10;
+
+if($variable >= $conditional_value) {
+
+	echo "El valor de la variable es mayor o igual a {$conditional_value}<br>";
+} else {
+	echo "El valor de la variable es menor que {$conditional_value}<br>";
+}
+
+echo "Si la condicion se cumple o no, siempre veras una linea mas que esta...";
+
+?>
