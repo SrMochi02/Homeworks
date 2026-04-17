4210a31407bb64f7580f11c62ba355f7a15bb6ad
Raul Hernandez <optimusgamer55hdz@gmail.com>
2026-04-17 09:15:27 -0600

    feat: added simple structure condition

---
+++ DPW/06_guia_practica/01example.php
@@ -0,0 +1,13 @@
+<?php
+
+$variable = 10;
+
+if($variable == 10) {
+
+	echo "El valor de la viarable es {$variable}<br>\n";
+} 
+else {
+	echo "Si la condicion no se cumple solo veras esta linea...";
+}
+
+?>
